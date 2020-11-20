32a51b99ec6aaf6ae6da6f1d0458126e62aeb01a
ilya veselov <ves.ilya@mail.ru>
2020-11-20 18:22:03 +0300

    M-02 Adding auth page

--- app/src/main/java/com/example/memes/AuthActivity.kt
+++ app/src/main/java/com/example/memes/AuthActivity.kt
@@ -2,10 +2,21 @@ package com.example.memes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import studio.carbonylgroup.textfieldboxes.TextFieldBoxes
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        val spinner = findViewById<ProgressBar>(R.id.progress_bar)
+        spinner.visibility = View.VISIBLE
+
+
+        val passwordInput = findViewById<TextFieldBoxes>(R.id.password_input)
+        passwordInput.helperText = getString(R.string.password_hint)
+        passwordInput.setError(getString(R.string.password_hint), true)
     }
 }
