e76b30c50d42cf2511c522a4dfc544bff125c644
ilya veselov <ves.ilya@mail.ru>
2020-11-28 14:52:48 +0300

    M-05 Tabs page added

--- app/src/main/java/com/example/memes/TabsActivity.kt
+++ app/src/main/java/com/example/memes/TabsActivity.kt
@@ -2,10 +2,32 @@ package com.example.memes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import studio.carbonylgroup.textfieldboxes.ExtendedEditText
 
 class TabsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tabs)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainContentFrame, PanelsFragment()).commit()
+
+        val navigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        navigation.setOnNavigationItemSelectedListener {
+            val selectedFragment = when (it.itemId) {
+                R.id.action_panels -> PanelsFragment()
+                R.id.action_add -> AddFragment()
+                R.id.action_profile -> ProfileFragment()
+                else -> null
+            }
+
+            if (selectedFragment != null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.mainContentFrame, selectedFragment).commit()
+            }
+
+            true
+        }
     }
 }
